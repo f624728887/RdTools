0cbdf11b70b6fc5634939469c4df784dff35ae4c
Rondo_dada <624728887@qq.com>
2020-04-02 08:18:22 +0800

    fix bug

--- RdToolsProject/RdTools/UIViewController+RdTools.h
+++ RdToolsProject/RdTools/UIViewController+RdTools.h
@@ -17,7 +17,7 @@
 - (UIButton *_Nonnull)rd_SetDefaultTitleBackBtn;
 - (UIButton *_Nonnull)rd_SetDefaultImgBackBtn;
 
-- (void)rd_SetControllerTitle:(NSString *_Nonnull)title textColor:(UIColor *_Nonnull)textColor textFont:(NSString *_Nullable)textFont textSize:(float)textSize;
+- (void)rd_SetTitle:(NSString *_Nonnull)title textColor:(UIColor *_Nonnull)textColor textFont:(NSString *_Nullable)textFont textSize:(float)textSize;
 
 // 左
 - (UIButton *_Nonnull)rd_SetLeftBtnJustTitle:(NSString *_Nonnull)title responder:(void (^_Nullable)(UIButton * _Nullable sender))block;
