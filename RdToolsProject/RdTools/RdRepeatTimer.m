fc8110e2f11871b00113cb3288d6fca905a4fa95
jiangxuefeng <f624728887@163.com>
2021-03-05 09:09:17 +0800

    循环timer初始化首次间隔时间修改，修改为与循环间隔一致。

--- RdToolsProject/RdTools/RdRepeatTimer.m
+++ RdToolsProject/RdTools/RdRepeatTimer.m
@@ -22,7 +22,7 @@
     
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     rdtimer.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
-    dispatch_source_set_timer(rdtimer.timer, dispatch_walltime(nil, 0), period * NSEC_PER_SEC, 0);
+    dispatch_source_set_timer(rdtimer.timer, dispatch_walltime(nil, period * NSEC_PER_SEC), period * NSEC_PER_SEC, 0);
     dispatch_source_set_event_handler(rdtimer.timer, block);
     rdtimer.isSuspend = YES;
     
